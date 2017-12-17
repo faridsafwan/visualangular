376f6984c9063103f23a35470fc8469aa2acb462
faridsafwan <farid.safwan@acceval-intl.com>
2017-12-17 20:41:16 +0800

    er

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   // template: `
   // '<app-server></app-server>
   //  <app-server></app-server>'`,
-   
+    
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
